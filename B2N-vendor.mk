11b0844ae5a7546bbbe575728f359d312efb84b2
Tuan Anh <tuan73176@gmail.com>
2025-03-24 23:44:38 +0700

    B2N: Drop prebuilt libgui_vendor
    Change-Id: Idcf3044c79a6d59283cd0e92378db897f39e82c0

--- B2N-vendor.mk
+++ B2N-vendor.mk
@@ -195,7 +195,6 @@ PRODUCT_PACKAGES += \
     libfih_camera_effect \
     libflash_dummy_flash \
     libflash_pmic \
-    libgui_vendor \
     libhal_dbg \
     libimage_matting \
     libjpegdhw \
